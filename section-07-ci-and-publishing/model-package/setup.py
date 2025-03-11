bca57faac84e317c7b0898e61b36a0db5f3e8ad7
Alessandro Cesa <a.cesa@circle.local>
2025-03-11 15:24:10 +0100

    incremented gemfury package version

--- section-07-ci-and-publishing/model-package/setup.py
+++ section-07-ci-and-publishing/model-package/setup.py
@@ -8,9 +8,9 @@ from setuptools import find_packages, setup
 # Package meta-data.
 NAME = 'tid-regression-model'
 DESCRIPTION = "Example regression model package from Train In Data."
-URL = "https://github.com/trainindata/testing-and-monitoring-ml-deployments"
-EMAIL = "christopher.samiullah@protonmail.com"
-AUTHOR = "ChristopherGS"
+URL = "https://github.com/AlessandroCesaTs/deploying-machine-learning-models.git"
+EMAIL = "alessandro.cesa22@gmail.com"
+AUTHOR = "alessandroCesaTs"
 REQUIRES_PYTHON = ">=3.9.1, <3.11.10"
 
 
